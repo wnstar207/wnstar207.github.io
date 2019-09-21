77c0ed6f25d7c76eba24ff7d78dd67f36a8ef53c
wnstar207 <55424277+wnstar207@users.noreply.github.com>
2019-09-21 00:31:13 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-function copyrt(){
+    
     var d = new Date();
     var n = d.getFullYear();
-    document.getElementById("cpy").innerHTML = n;
-}
-let lastModif = new Date(document.lastModified); 
+    document.getElementById("year").innerHTML = n;
