d7771a72094634fb3e4f020666ee018a261683fc
wnstar207 <55424277+wnstar207@users.noreply.github.com>
2019-09-21 01:00:56 -0700

    Add files via upload

--- modified.js
+++ modified.js
@@ -1,6 +1,6 @@
 
     var modified = new Date(document.lastModified);
-    var month = modified.getMonth() + 1;
-    var date = modified.getDate();
-    var year = modified.getFullYear();
+    month = modified.getMonth() + 1;
+    date = modified.getDate();
+    year = modified.getFullYear();
     document.getElementById("update").textContent = (month + " " + date + ", " + year);
