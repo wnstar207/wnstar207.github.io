db684829e5fb59d9d5e3197d14a84e1589fb0e8d
wnstar207 <55424277+wnstar207@users.noreply.github.com>
2019-09-21 01:21:51 -0700

    Add files via upload

--- modified.js
+++ modified.js
@@ -1,20 +1,6 @@
-let monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
 
     let modified = new Date(document.lastModified);
-    var month = monthNames[modified.getMonth()];
+    let month = modified.getMonth();
     let date = modified.getDate();
     let year = modified.getFullYear();
-    document.getElementById("update").textContent = (month + " " + date + ", " + year);
+    document.getElementById("update").textContent = (month + "-" + date + "-" + year);
