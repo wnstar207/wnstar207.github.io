67f77d4cda9db2f38cefb097745862f741198d42
wnstar207 <wnstar207@gmail.com>
2019-10-23 13:33:20 -0700

    update

---
+++ lesson5/preston.js
@@ -0,0 +1,33 @@
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+var today = new Date();
+var nowDay = dayNames[today.getDay()];
+var nowDate = today.getDate();
+var nowMonth = monthNames[today.getMonth()];
+var nowYear = today.getFullYear();
+document.getElementById("today").innerHTML = nowDay + ", " + nowDate + " " + nowMonth + " " + nowYear;
+
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("year").innerHTML = n;
