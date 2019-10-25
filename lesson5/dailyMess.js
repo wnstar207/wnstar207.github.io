9e6087b6881c50f27c70d978a1e6d760387e27ed
wnstar207 <wnstar207@gmail.com>
2019-10-24 20:20:49 -0700

    update

--- lesson5/dailyMess.js
+++ lesson5/dailyMess.js
@@ -1,23 +1,12 @@
 var mesg = new Date();
 var dailyMesg = mesg.getDay();
-if (dailyMesg == 0) {
-    document.getElementById("dailyMess").innerHTML = "Happy Sunday";
-}
-    else if (dailyMesg == 1) {
-        document.getElementById("dailyMess").innerHTML = "Happy Monday";
+if (dailyMesg === 0) {
+    document.getElementById("dailyMess").innerHTML = "Have a good weekend!";
     }
-        else if (dailyMesg == 2) {
-            document.getElementById("dailyMess").innerHTML = "Happy Tuesday";
+    else if (dailyMesg === 6) {
+        document.getElementById("dailyMess").innerHTML = "Have a good weekend!";
         }
-            else if (dailyMesg == 3) {
-                document.getElementById("dailyMess").innerHTML = "Happy Wednesday";
+        else if (dailyMesg === 4) {
+            document.getElementById("dailyMess").innerHTML = "Is it Friday yet?"
             }
-                else if (dailyMesg == 4) {
-                    document.getElementById("dailyMess").innerHTML = "Happy Thursday";
-                }
-                    else if (dailyMesg == 5) {
-                        document.getElementById("dailyMess").innerHTML = "Happy Friday";
-                    }
-                        else {
-                            document.getElementById("dailyMess").innerHTML = "Happy Saturday";
-                        };
+            else {}
