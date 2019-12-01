628bd0816080d870ba1886e361b6a9778847501b
wnstar207 <wnstar207@gmail.com>
2019-11-30 16:16:03 -0800

    update

--- lesson11/scripts/fish-haven-events.js
+++ lesson11/scripts/fish-haven-events.js
@@ -6,22 +6,25 @@ fetch(requestURL)
     .then(function (jsonObject) {
         const towns = jsonObject["towns"];
         for (let i=0; i<towns.length; i++) {
-            if (towns[i].name == "Preston") {
+            if (towns[i].name == "Fish Haven") {
                 let card = document.createElement("section");
                 let h3 = document.createElement("h2");
                 let p1 = document.createElement("h4");
                 let p2 = document.createElement("h4");
                 let p3 = document.createElement("h4");
+                let p4 = document.createElement("h4");
 
-                h3.textContent = "Upcoming Events for Preston";
+                h3.textContent = "Upcoming Events for Fish Haven";
                 p1.textContent = towns[i].events[0];
                 p2.textContent = towns[i].events[1];
                 p3.textContent = towns[i].events[2];
+                p4.textContent = towns[i].events[3];
 
                 card.appendChild(h3);
                 card.appendChild(p1);
                 card.appendChild(p2);
                 card.appendChild(p3);
+                card.appendChild(p4);
                 
                 document.querySelector("div.events").appendChild(card);
 
--- lesson11/scripts/soda-springs-events.js
+++ lesson11/scripts/soda-springs-events.js
@@ -6,14 +6,14 @@ fetch(requestURL)
     .then(function (jsonObject) {
         const towns = jsonObject["towns"];
         for (let i=0; i<towns.length; i++) {
-            if (towns[i].name == "Preston") {
+            if (towns[i].name == "Soda Springs") {
                 let card = document.createElement("section");
                 let h3 = document.createElement("h2");
                 let p1 = document.createElement("h4");
                 let p2 = document.createElement("h4");
                 let p3 = document.createElement("h4");
 
-                h3.textContent = "Upcoming Events for Preston";
+                h3.textContent = "Upcoming Events for Soda Springs";
                 p1.textContent = towns[i].events[0];
                 p2.textContent = towns[i].events[1];
                 p3.textContent = towns[i].events[2];
