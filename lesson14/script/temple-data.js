eba7d63a88ede95d9bb0556a85a58ecd5dfbeb71
wnstar207 <wnstar207@gmail.com>
2019-12-09 16:39:59 -0800

    update

--- lesson14/script/temple-data.js
+++ lesson14/script/temple-data.js
@@ -11,12 +11,22 @@ let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let p5 = document.createElement('p');
+        let p6 = document.createElement('p');
   h2.textContent = temples[i].name;
-        p1.textContent = "Date of Birth: " + temples[i].address + '<br>' 
-        p2.textContent = "Place of Birth: " + prophets[i].city;
+        p1.textContent = temples[i].address
+        p2.textContent = temples[i].city; 
+        p3.textContent = "Phone: " + temples[i].phone;
+        p4.textContent = "Temple Services";
+        p5.textContent = temples[i].templeservices;
   card.appendChild(h2);
         card.appendChild(p1);
         card.appendChild(p2);
+        card.appendChild(p3);
+        card.appendChild(p4);
+        card.appendChild(p5);
 document.querySelector('div.cards').appendChild(card);
 }	
 });
