5f753a9c15b8a75aee747717b54f070a6af56706
wnstar207 <wnstar207@gmail.com>
2020-02-07 18:55:26 -0800

    update

--- week5/w5-script/dailyMess.js
+++ week5/w5-script/dailyMess.js
@@ -1,12 +1,15 @@
 var mesg = new Date();
 var dailyMesg = mesg.getDay();
-if (dailyMesg === 0) {
+if (dailyMesg == 0) {
     document.getElementById("dailyMess").innerHTML = "Have a good weekend!";
     }
-    else if (dailyMesg === 6) {
+    else if (dailyMesg == 6) {
         document.getElementById("dailyMess").innerHTML = "Have a good weekend!";
         }
-        else if (dailyMesg === 4) {
+        else if (dailyMesg == 4) {
             document.getElementById("dailyMess").innerHTML = "Is it Friday yet?"
+            }
+            else if (dailyMesg == 5) {
+                document.getElementById("dailyMess").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
             }
             else {}
